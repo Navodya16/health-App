b03523ec544a09ac31bdf3baf1aea87449d26c9e
Navodya Rathnasekara <prabuddhi.rath2@gmail.com>
2022-11-08 02:45:50 +0530

    connected backend with frontend

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Container,
   Form,
@@ -6,12 +7,16 @@ import {
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Health App</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/">Health App</Link>
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
@@ -25,7 +30,9 @@ const Header = () => {
           </Nav>
 
           <Nav>
-            <Nav.Link href="#home">My Notes</Nav.Link>
+            <Nav.Link href="/mynotes">
+              <Link to="/mynotes">My Notes</Link>
+            </Nav.Link>
             <NavDropdown title="Navodya" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
               <NavDropdown.Divider />
